28c709ab4e91047d2002742b640f606bc64a216d
Galvarine3 <ariel.wt.93@gmail.com>
2026-02-18 20:09:46 -0300

    Delete migrations directory

--- migrations/20251115005151_add_message_postid/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Message" ADD COLUMN     "postId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Message" ADD CONSTRAINT "Message_postId_fkey" FOREIGN KEY ("postId") REFERENCES "CommunityPost"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- migrations/20251124193500_add_email_verification/migration.sql
+++
@@ -1,5 +0,0 @@
--- Add email verification fields to User
-ALTER TABLE "User"
-  ADD COLUMN "emailVerified" BOOLEAN NOT NULL DEFAULT false,
-  ADD COLUMN "verificationToken" TEXT,
-  ADD COLUMN "verificationSentAt" TIMESTAMP(3);
